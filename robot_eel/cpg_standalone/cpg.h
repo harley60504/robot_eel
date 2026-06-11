3dbaadda94259b386fbe244a913adbdb2cb94b0b
harley60504 <147987913+harley60504@users.noreply.github.com>
2026-06-11 15:10:15 +0800

    Align standalone CPG implementation with control firmware

--- robot_eel/cpg_standalone/cpg.h
+++ robot_eel/cpg_standalone/cpg.h
@@ -8,114 +8,73 @@ inline float wrap_pi(float x) {
   return x;
 }
 
-inline float degToRad(float deg) {
-  return deg * ((float)M_PI / 180.0f);
+inline float getPhaseOffset(int j) {
+  float offset = 0.0f;
+  for (int i = 0; i < j && i < bodyNum - 1; i++) {
+    offset -= phaseLags[i];
+  }
+  return offset;
 }
 
-inline float radToDeg(float rad) {
-  return rad * (180.0f / (float)M_PI);
+inline float getNeighborDesiredDelta(int leftJoint, int rightJoint) {
+  return getPhaseOffset(leftJoint) - getPhaseOffset(rightJoint);
 }
 
-// Python: _target_delta()
-// target_delta = 1.0 / (wavelength * body_length)
-inline float getTargetDelta() {
-  float lambdaInput = wavelength * bodyLength;
-  if (lambdaInput < 1e-6f) lambdaInput = 1e-6f;
-  return 1.0f / lambdaInput;
-}
-
-// Python: _phase_offsets()
-// If phase_lags exists, it takes priority over wavelength.
 inline void computePhaseOffsets(float offsets[bodyNum]) {
   offsets[0] = 0.0f;
-
-  if (usePhaseLags) {
-    float sum = 0.0f;
-    for (int j = 1; j < bodyNum; j++) {
-      sum += phaseLags[j - 1];
-      offsets[j] = -sum;
-    }
-  } else {
-    float targetDelta = getTargetDelta();
-    for (int j = 1; j < bodyNum; j++) {
-      offsets[j] = -((float)j) * targetDelta;
-    }
-  }
-}
-
-// Python: _mu_targets()
-// amp_scales are converted to mu scales by squaring.
-inline void computeMuTargets(float muTargets[bodyNum]) {
-  for (int j = 0; j < bodyNum; j++) {
-    if (useAmpScales) {
-      float s = ampScales[j];
-      if (s < 0.0f) s = 0.0f;
-      muTargets[j] = muBase * s * s;
-    } else {
-      muTargets[j] = muBase;
-    }
-  }
-}
-
-// Python: reset()
-// r[:] = 0.25
-// theta[:] = phase_offsets
-inline void resetCPG() {
-  float phaseOffsets[bodyNum];
-  computePhaseOffsets(phaseOffsets);
-
-  for (int j = 0; j < bodyNum; j++) {
-    cpg[j].r     = 0.25f;
-    cpg[j].theta = phaseOffsets[j];
-    cpg[j].alpha = alphaHopf;
-    cpg[j].mu    = muBase;
+  for (int j = 1; j < bodyNum; j++) {
+    offsets[j] = offsets[j - 1] - phaseLags[j - 1];
   }
 }
 
 inline void initCPG() {
-  resetCPG();
-}
+  float phaseOffsets[bodyNum];
+  computePhaseOffsets(phaseOffsets);
 
-// Python: output()
-// return ajoint * r * cos(theta) + joint_bias
-inline float getCPGOutputRad(int j) {
-  float biasRad = 0.0f;
-
-  if (useJointBiasDeg) {
-    biasRad = degToRad(jointBiasDeg[j]);
+  // Match Python HopfCPG.reset(): r starts at 0.25 and theta starts at phase offsets.
+  for (int j = 0; j < bodyNum; j++) {
+    cpg[j].r = 0.25f;
+    cpg[j].theta = phaseOffsets[j];
+    cpg[j].alpha = 4.0f;
+    cpg[j].mu = ampScales[j] * ampScales[j];
   }
-
-  return ajointRad * cpg[j].r * cosf(cpg[j].theta) + biasRad;
 }
 
-inline float getCPGOutputDeg(int j) {
-  return radToDeg(getCPGOutputRad(j));
+inline float getCPGOutput(int j) {
+  return Ajoint * cpg[j].r * cosf(cpg[j].theta) + jointBiasDeg[j];
 }
 
-// Python: step()
-// This uses old_r / old_theta snapshots, so all joints update synchronously.
-inline void updateCPGAll(float t, float dt) {
-  float omega = 2.0f * (float)M_PI * frequency;
+inline float getLambdaInput() { return fmaxf(lambda * L, 1e-6f); }
+inline float getTargetDelta() { return 1.0f / getLambdaInput(); }
+
+inline void updateCPGAll(float t, float dt, float fb_phase, float fb_amp) {
+  const float K_couple   = 0.35f;
+  const float K_anchor   = 0.10f;
+  const float k_fb_phase = 0.8f;
+  const float k_fb_amp   = 0.25f;
+
+  float omega = 2.0f * M_PI * frequency;
 
   float phaseOffsets[bodyNum];
-  float muTargets[bodyNum];
-
   float oldR[bodyNum];
   float oldTheta[bodyNum];
-
   float dr[bodyNum];
   float dtheta[bodyNum];
+  float muTargets[bodyNum];
 
   computePhaseOffsets(phaseOffsets);
-  computeMuTargets(muTargets);
 
+  // Python equivalent:
+  // old_r = self.r.copy()
+  // old_theta = self.theta.copy()
   for (int j = 0; j < bodyNum; j++) {
     oldR[j] = cpg[j].r;
     oldTheta[j] = cpg[j].theta;
+    muTargets[j] = ampScales[j] * ampScales[j];
   }
 
   for (int j = 0; j < bodyNum; j++) {
-    dr[j] = alphaHopf * (muTargets[j] - oldR[j] * oldR[j]) * oldR[j];
+    dr[j] = cpg[j].alpha * (muTargets[j] - oldR[j] * oldR[j]) * oldR[j];
     dtheta[j] = omega;
   }
 
@@ -123,32 +82,34 @@ inline void updateCPGAll(float t, float dt) {
     if (j - 1 >= 0) {
       float desiredL = phaseOffsets[j - 1] - phaseOffsets[j];
       float errL = wrap_pi((oldTheta[j - 1] - oldTheta[j]) - desiredL);
-      dtheta[j] += kCouple * sinf(errL);
+      dtheta[j] += K_couple * sinf(errL);
     }
 
     if (j + 1 < bodyNum) {
       float desiredR = phaseOffsets[j + 1] - phaseOffsets[j];
       float errR = wrap_pi((oldTheta[j + 1] - oldTheta[j]) - desiredR);
-      dtheta[j] += kCouple * sinf(errR);
+      dtheta[j] += K_couple * sinf(errR);
     }
 
-    float thRef = omega * t + phaseOffsets[j];
-    float eRef = wrap_pi(thRef - oldTheta[j]);
-    dtheta[j] += kAnchor * sinf(eRef);
+    float th_ref = omega * t + phaseOffsets[j];
+    float e_ref = wrap_pi(th_ref - oldTheta[j]);
+    dtheta[j] += K_anchor * sinf(e_ref);
   }
 
   for (int j = 0; j < bodyNum; j++) {
-    dtheta[j] += kFbPhase * fbPhase;
-    dr[j]     += kFbAmp   * fbAmp;
+    dtheta[j] += k_fb_phase * fb_phase;
+    dr[j]     += k_fb_amp   * fb_amp;
   }
 
   for (int j = 0; j < bodyNum; j++) {
-    cpg[j].r = oldR[j] + dr[j] * dt;
-    if (cpg[j].r < 0.0f) cpg[j].r = 0.0f;
-
+    cpg[j].r = fmaxf(0.0f, oldR[j] + dr[j] * dt);
     cpg[j].theta = wrap_pi(oldTheta[j] + dtheta[j] * dt);
-
-    cpg[j].alpha = alphaHopf;
     cpg[j].mu = muTargets[j];
   }
+}
+
+// Backward-compatible single-joint updater. Prefer updateCPGAll() for CPG mode.
+inline void updateCPG(float t, float dt, int j, float fb_phase, float fb_amp) {
+  (void)j;
+  updateCPGAll(t, dt, fb_phase, fb_amp);
 }
